dc33a1540ce3d5d1c7c23c5a86c762dd6d6e29f9
alanzhangg <alanzhangg@icloud.com>
2017-10-13 18:13:46 +0800

    add project

---
+++ MyPlayground.playground/Pages/text.xcplaygroundpage/Contents.swift
@@ -0,0 +1,54 @@
+//: [Previous](@previous)
+//: #### Second Level Heading
+//: ### Second Level Heading
+//: ## Second Level Heading
+//: # Second Level Heading
+//: Find more information for [swift](http://swift.org)
+//: Meet Swiftie the cat ![swift the cat](hua.png "nihao")the real boss.
+/*:
+* Callout(Llama Spotting Tips):
+# sections
+- Important: 1. item1 one line
+- Section2: 2. *item2* **item2** ***item2***
+*/
+
+/*:
+	* Item 1, List A
+	* Item 2, List A
+	* Item 1, List B
+	* Item 1, List C
+
+	for character in "Aesop" {
+		print(character)}
+	}
+
+*/
+
+//: Show Swift elements such as `for` and `let` as monspaced code font
+
+//:***
+
+/*:
+	Setup and use a link reference.
+	
+	[The Swift Programming Language]: http://developer.apple.com/library/ios/documentation/Swift/Conceptual/Swift_Programming_Language/  "Some hover text"
+
+	For light reading we suggest [The Swift Programming Language].
+
+	For more information, see [The Swift Programming Language].
+
+	If you are not sure, please see [The Swift Programming Language].
+*/
+
+import Foundation
+import UIKit
+import PlaygroundSupport
+
+let myView = UIViewController()
+PlaygroundPage.current.liveView = myView
+
+
+var str = "Hello, playground"
+print(string)
+
+//: [text](@next)
